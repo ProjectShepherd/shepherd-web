8640ec20c81442111c261801729a1da02e61b758
Nate Brennand <natebrennand@gmail.com>
2013-09-13 21:39:03 -0700

    add /report form

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 
 var app = angular.module('app', [
     'app.home',
-    'app.report'
+    'app.report',
+    'app.providers'
 ]);
 
 app.config( ['$routeProvider', function ($routeProvider) {
     $routeProvider
 
+        .when('/create',{templateUrl: 'partials/report.html',   controller: 'Report'})
         .when('/',      {templateUrl: 'partials/home.html',     controller: 'Home'});
 }]);
--- js/controllers/Report.js
+++ js/controllers/Report.js
@@ -1,6 +1,16 @@
 
 angular.module('app.report', [])
 
-.controller('Report', function ($scope) {
+.controller('Report', function ($scope, FormList) {
+    console.log('MAKING A REPORT');
+    $scope.eyeColors = FormList.eyes;
+    $scope.hairColors = FormList.hair;
+    $scope.raceChoices = FormList.race;
+    $scope.report = {};
 
+    $scope.report.sex = 'M';
+
+    $scope.makeReport = function () {
+        console.log('REPORT MADE');
+    };
 });
